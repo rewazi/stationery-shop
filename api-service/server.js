d704c0b8c97651e1715e85bbf164975b55310764
Ivan <krakeen99@gmail.com>
2026-09-08 08:45:56 +0300

    Add Express server with API endpoint for team info

--- api-service/server.js
+++ api-service/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.get('/api/info', (req, res) => {
+  const teamName = process.env.TEAM_NAME;
+  
+  if (!teamName) {
+    return res.json({ meeskond: "Tundmatu tiim (Viga!)" });
+  }
+
+  res.json({ meeskond: teamName });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
